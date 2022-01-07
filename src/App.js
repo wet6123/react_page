fcb90896c3cb2746cd0bdb5f9801fa0f6530072d
JunSeong Bae <gentlmanjun@naver.com>
2022-01-07 23:43:26 +0900

    탭 메뉴 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
 import React, { useEffect, useState } from "react";
 import "./css/App.css";
+// import spiderTom from "../public/img/7.jpg";
+
+//탭메뉴 콘텐츠
+const content = [
+  {
+    tab: "톰 홀랜드",
+    content: "마블 시네마틱 유니버스의 스파이더맨",
+    // img: spiderTom,
+  },
+  {
+    tab: "베네딕트 컴버배치",
+    content: "마블 시네마틱 유니버스의 닥터 스트레인지",
+  },
+  {
+    tab: "토비 맥과이어",
+    content: "스파이더맨 트릴로지의 스파이더맨",
+  },
+  {
+    tab: "앤드루 가필드",
+    content: "어메이징 스파이더맨 시리즈의 스파이더맨",
+  },
+];
+
+//탭메뉴 hooks
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
+  };
+};
 
 function App() {
   const [position, setposition] = useState(0);
@@ -13,6 +44,8 @@ function App() {
       window.removeEventListener("scroll", onScroll);
     };
   }, []);
+
+  const { currentItem, changeItem } = useTabs(0, content);
 
   return (
     <div>
@@ -43,36 +76,93 @@ function App() {
           }}
         />
       </div>
-      <div>
+
+      <div class="letters">
+        <div class="bigLetter">
+          <p
+            style={{
+              opacity: (position - 2700) / 200,
+            }}
+          >
+            시리즈 최초로 정체가 탄로난 스파이더맨!
+          </p>
+          <p
+            style={{
+              opacity: (position - 2900) / 200,
+            }}
+          >
+            절체절명의 위기에 빠진 스파이더맨 피터 파커!
+          </p>
+          <p
+            style={{
+              opacity: (position - 3100) / 200,
+            }}
+          >
+            모든 일상을 빼앗아간 거대한 위협과 맞서다!
+          </p>
+        </div>
+
         <p
+          class="synop"
           style={{
-            opacity: (position - 2150) / 200,
+            opacity: (position - 3900) / 200,
           }}
         >
-          시리즈 최초로 정체가 탄로난 스파이더맨!
-        </p>
-        <p
-          style={{
-            opacity: (position - 2200) / 200,
-          }}
-        >
-          절체절명의 위기에 빠진 스파이더맨 피터 파커!
-        </p>
-        <p
-          style={{
-            opacity: (position - 2420) / 200,
-          }}
-        >
-          모든 일상을 빼앗아간 거대한 위협과 맞서다!
-        </p>
-        <p id="synop">
           ‘미스테리오’의 계략으로 세상에 정체가 탄로난 스파이더맨 ‘피터 파커’는
-          하루 아침에 평범한 일상을 잃게 된다. 문제를 해결하기 위해 ‘닥터
-          스트레인지’를 찾아가 도움을 청하지만 뜻하지 않게 멀티버스가 열리면서
-          각기 다른 차원의 불청객들이 나타난다. ‘닥터 옥토퍼스’를 비롯해
-          스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에 ‘피터 파커’는
-          사상 최악의 위기를 맞게 되는데…
         </p>
+        <p
+          class="synop"
+          style={{
+            opacity: (position - 4000) / 200,
+          }}
+        >
+          하루 아침에 평범한 일상을 잃게 된다. 문제를 해결하기 위해
+        </p>
+        <p
+          class="synop"
+          style={{
+            opacity: (position - 4100) / 200,
+          }}
+        >
+          ‘닥터스트레인지’를 찾아가 도움을 청하지만 뜻하지 않게 멀티버스가
+        </p>
+        <p
+          class="synop"
+          style={{
+            opacity: (position - 4200) / 200,
+          }}
+        >
+          열리면서 각기 다른 차원의 불청객들이 나타난다. ‘닥터 옥토퍼스’를
+        </p>
+        <p
+          class="synop"
+          style={{
+            opacity: (position - 4300) / 200,
+          }}
+        >
+          비롯해 스파이더맨에게 깊은 원한을 가진 숙적들의 강력한 공격에
+        </p>
+
+        <p
+          class="synop"
+          style={{
+            opacity: (position - 4400) / 200,
+          }}
+        >
+          ‘피터 파커’는 사상 최악의 위기를 맞게 되는데…
+        </p>
+      </div>
+
+      <div
+        class="tabMenu"
+        style={{
+          opacity: (position - 5000) / 200,
+        }}
+      >
+        {content.map((section, index) => (
+          <button onClick={() => changeItem(index)}>{section.tab}</button>
+        ))}
+        <div>{currentItem.content}</div>
       </div>
     </div>
   );
